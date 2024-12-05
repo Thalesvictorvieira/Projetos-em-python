210a36dbf07ebd4b97d7aa6deda17ce6ef2b3876
Thales victor vieira <114888655+Thalesvictorvieira@users.noreply.github.com>
2024-12-05 11:33:32 -0300

    dnv

---
+++ jogo_da_cobrinha/virtual/Scripts/classes.py
@@ -0,0 +1,8 @@
+class objeto:
+    def __init__(self,posicao_x,posicao_y,cor_obj,largura_obj,altura_obj):
+        self.posicao_x = posicao_x
+        self.posicao_y = posicao_y
+        self.cor_obj = cor_obj
+        self.largura_obj = largura_obj
+        self.altura_obj = altura_obj
+    
